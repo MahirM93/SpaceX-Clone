edc3a1a81bfaa4f702a224fe9be4bc7db61a2455
rails s <mesanovicmahir@gmail.com>
2022-08-26 17:18:08 +0200

    Naredba1

---
+++ index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport"content="width=device-width,initial-scale=1.0"/>
+    <link rel="stylesheet"href="style.css">
+    <script src="js/script.js"defer></script>
+</head>
+
+<body>
+    <header class="main-header">
+    <div class="logo">
+        <a href="index.php">
+        <img src="slike/r.png"alt="spaceX"/>
+        </a>
+    </div>
+    <nav class="meni">
+        <ul>
+            <li><a href="falcon9.html">Falcon9</a></li>
+            <li><a href="falcon-hevy.html">Falcon Heavy</a> </li>
+            <li><a href="/">Starship</a></li>
+            <li><a href="/">Human Space Flight</a></li>
+            <li><a href="/">Rideshear</a></li>
+            <li><a href="/">Shop</a></li>
+        </ul>
+
+    </nav>
+
+    </header>
+
+    <!---SECTION A -->
+    <section class="section-a">
+    <div class="section-inner">
+        <h4>Upcoming Lanch</h4>
+        <h2>CRS-25 Mision</h2>
+        <a href="#" class="btn">
+        <div class="hover"></div>
+        <span>Rewatch</span></a>
+    </div>
+
+    <div class="scroll-arrow">
+        <svg width="30px" heigh="20px">
+         <path stroke="#ffffff" fill="none" stroke-width="2px" d="M2.000,5.000 L15.000,18.000 L28.000,5.000"></path>
+        </svg>
+    </div>
+    </section>
+
+
+    <!---SECTION B -->
+    <section class="section-b">
+    <div class="section-inner">
+        <h4>Recent Lanch</h4>
+        <h2>Starlink Mission</h2>
+        <a href="#" class="btn">
+        <div class="hover"></div>
+        <span>Rewatch</span></a>
+    </div>
+
+    <div class="scroll-arrow">
+        <svg width="30px" heigh="20px">
+         <path stroke="#ffffff" fill="none" stroke-width="2px" d="M2.000,5.000 L15.000,18.000 L28.000,5.000"></path>
+        </svg>
+    </div>
+    </section>
+
+     <!---SECTION C -->
+     <section class="section-c">
+    <div class="section-inner">
+        <h4>Recent Mission</h4>
+        <h2>SES Mision</h2>
+        <a href="#" class="btn">
+        <div class="hover"></div>
+        <span>Rewatch</span></a>
+    </div>
+
+    <div class="scroll-arrow">
+        <svg width="30px" heigh="20px">
+         <path stroke="#ffffff" fill="none" stroke-width="2px" d="M2.000,5.000 L15.000,18.000 L28.000,5.000"></path>
+        </svg>
+    </div>
+    </section>
+
+     <!---SECTION D -->
+     <section class="section-d">
+    <div class="section-inner">
+        <h4>Recent Lanch</h4>
+        <h2>Globalstar FM15 Mission</h2>
+        <a href="#" class="btn">
+        <div class="hover"></div>
+        <span>Rewatch</span></a>
+    </div>
+
+    <div class="scroll-arrow">
+        <svg width="30px" heigh="20px">
+         <path stroke="#ffffff" fill="none" stroke-width="2px" d="M2.000,5.000 L15.000,18.000 L28.000,5.000"></path>
+        </svg>
+    </div>
+    </section>
+     <!---SECTION E -->
+     <section class="section-e">
+    <div class="section-inner">
+        
+        <h2>Starship update</h2>
+        <a href="#" class="btn">
+        <div class="hover"></div>
+        <span>Rewatch</span></a>
+    </div>
+
+    <div class="scroll-arrow">
+        <svg width="30px" heigh="20px">
+         <path stroke="#ffffff" fill="none" stroke-width="2px" d="M2.000,5.000 L15.000,18.000 L28.000,5.000"></path>
+        </svg>
+    </div>
+    </section>
+
+     <!---SECTION F -->
+     <section class="section-f">
+    <div class="section-inner">
+      
+        <h2>Starship to add NASA on Moon</h2>
+        <a href="#" class="btn">
+        <div class="hover"></div>
+        <span>Rewatch</span></a>
+    </div>
+
+    
+    </section>
+    <footer>
+        <ul>
+            <li>Mešanović Mahir 2022 &copy;</li>
+        </ul>
+    </footer>
+
+</body>
+
+
+
+
+</html>
